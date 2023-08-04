66ebf48f3c08b499e85d074a9a06ccdf5ccb59fa
asdru333 <asdrubal.villegas.molina@hotmail.com>
2023-08-03 23:41:49 -0600

    test

--- src/navbar/navbar.component.ts
+++ src/navbar/navbar.component.ts
@@ -19,7 +19,7 @@ export class NavbarComponent implements OnInit {
   constructor() {
     this.isOpenMenu = false;
     this.isOpenServices = false;
-    this.isLogin = true;
+    this.isLogin = false;
     this.linksOne = [
       {name:"HOME",link:""},
       {name:"SOBRE NOSOTROS",link:"/about"},
